0fe070fcdbac8283ff73ae48248b6a93ce6580b1
Vishal Vishwakarma <vishal.v@tripfactory.travel>
2026-09-14 14:09:12 +0530

    anchor init

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("2KRXzBtBcv9dgKDx2KGF84N8XCHdJMLMse97hzYFpNMF");
+
+#[program]
+pub mod amm_q3_w3 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        crate::instructions::initialize::handle_initialize(ctx)
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        crate::instructions::increment::handle_increment(ctx)
+    }
+}
