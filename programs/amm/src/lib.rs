0b5e609f3a582b89a4d21a49884e4f81e225b469
Vishal Vishwakarma <vishal.v@tripfactory.travel>
2026-09-15 12:55:39 +0530

    completed deposit instruction

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -19,4 +19,8 @@ pub mod amm_q3_w3 {
         ctx.accounts.handle_initialize(ctx.bumps.pool);
         Ok(())
     }
+
+    pub fn deposit(ctx: Context<Deposit>, amount_a: u64, amount_b: u64) -> Result<()> {
+        ctx.accounts.handle_deposit(amount_a, amount_b)
+    }
 }
